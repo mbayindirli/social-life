57bc898ef337343cc3fa2b23412faea397ae550a
mbayindirli <mustafabayindirli27@gmail.com>
2020-12-27 15:10:51 +0300

    UserSignUpPage sayfasında düzenleme

--- src/UserSignUpPage.js
+++ src/UserSignUpPage.js
@@ -7,48 +7,32 @@ displayName:null,
 password:null,
 passwordRepeat:null
 }
-onChangeUserName=event=>{
+onChange=event=>{
+const{name,value}=event.target
 this.setState({
-username:event.target.value
-})
-}
-onChangePassword=event=>{
-this.setState({
-password:event.target.value
+[name]:value
 })
 }
 
-onChangePasswordRepeat=event=>{
-this.setState({
-passwordRepeat:event.target.value
-})
-}
-
-
-onChangeDisplayName=event=>{
-this.setState({
-displayName:event.target.value
-})
-}
 //her class component render fonksionunu illaki owerride etmeli
 render(){return(
 <form>
   <h1 align= "center">Sign Up</h1>
   <div>
    <label>Username</label>
-   <input onChange={this.onChangeUserName} />
+   <input name="username" onChange={this.onChange} />
   </div>
   <div>
      <label>Display Name</label>
-     <input onChange={this.onChangeDisplayName}/>
+     <input name="displayName" onChange={this.onChange}/>
   </div>
    <div>
        <label>Password</label>
-       <input type="password" onChange={this.onChangePassword}/>
+       <input name="password" type="password" onChange={this.onChange}/>
    </div>
    <div>
           <label>Repeat Password</label>
-          <input type="password" onChange={this.onChangePasswordRepeat}/>
+          <input name ="passwordRepeat" type="password" onChange={this.onChange}/>
    </div>
    <div>
     <button>Sign Up</button>
