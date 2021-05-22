3a367441a7ddf4f535f4c78e9b0036d2d8221bb7
mbayindirli <mustafabayindirli27@gmail.com>
2021-05-22 12:39:57 +0300

    Login page sayfasına spinner eklendi Log seviyesi debug a cekildi

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import {changeLanguage,login} from '../api/apiCalls'
 import {withTranslation} from 'react-i18next';
+import {axios} from 'axios'
 class LoginPage extends React.Component{
 state={
 username:null,
-password:null
+password:null,
+error:null,
+pandingApiCall:false
 }
 
 onChange=event=>{
 const{name,value}=event.target;
 this.setState({
-[name]:value
+[name]:value,
+error:null
 })
 }
 //basic auth decode edilen bir yapısı vardır bu yüzden tercih edilmez
-onClickLogin=event=>{
+onClickLogin = async event=>{
 event.preventDefault();//form başka yerlere submit edilmesimesinin önüne geciliyor
 const creds={
 username:this.state.username,
 password:this.state.password
 };
-login(creds);
+try{
+this.setState({pandingApiCall:true})
+await login(creds);
+}catch(error){
+this.setState({
+error:error.response.data.message
+});
+}
+this.setState({pandingApiCall:false});
 }
 
 onChangeLanguage=language=>{
@@ -28,8 +40,21 @@ const{i18n}=this.props;
 i18n.changeLanguage(language);
 changeLanguage(language);
 }
+//componentDidMount(){
+//axios.interceptors.request.use((request) => {
+//    if (request) {
+//        //perform the manipulation here and change the response object
+//    }
+//    return request;
+//}, (error) => {
+//    return Promise.reject(error.message);
+//});
+//
+//}
 
-render(){return(
+render(){
+const enableLoginButton=this.state.password&&this.state.username
+return(
 <div className="container">
 <form>
  <h1 align= "center">{this.props.t('Login')}</h1>
@@ -46,10 +71,15 @@ render(){return(
      </div>
 
       <div className="text-center">
-         <button className="primary"onClick={this.onClickLogin}>
-           {this.props.t('Login')}
+         <button disabled={this.state.pandinpApiCall} className="primary"onClick={this.onClickLogin}
+         disabled={!enableLoginButton}>
+         {this.state.pandingApiCall?<span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>:''}
+        {this.props.t('Login')}
          </button>
         </div>
+        {this.state.error&&<div class="alert alert-danger" role="alert">
+         {this.state.error}
+        </div>}
         <div>
         <img src="https://www.countryflags.io/tr/flat/64.png"alt="Turkish Flag"onClick={()=>this.onChangeLanguage('tr')}></img>
          <img src="https://www.countryflags.io/US/flat/64.png"alt="US Flag"onClick={()=>this.onChangeLanguage('en')}></img>
